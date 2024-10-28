1df5294456ed051e2b729f3ce70fa27632c43fbe
joaoalura2008 <00001101711206sp@al.educacao.sp.gov.br>
2024-10-27 23:31:48 -0300

    teste 01 site

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História/política',
+    'O que é o Comunismo?',
+    'O O comunismo é uma ideologia de caráter político e socioeconômico que propõe a superação do capitalismo e a construção de uma sociedade baseada no senso de igualdade.'
+)
+
+criaCartao(
+    'História',
+    'Qual a foi a primeira capital do Brasil?',
+    'A primeira capital foi Salvador'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'política',
+    'O que é o BRICS?',
+    'O Brics é um mecanismo internacional de cooperação econômica e desenvolvimento formado por importantes economias emergentes.'
+)
